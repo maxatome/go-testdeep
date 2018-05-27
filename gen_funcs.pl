517e4e6685a30cc1c13782a0b6a5dfcdbbc91b18
Maxime Soulé <btik-git@scoubidou.com>
2018-05-27 10:28:00 +0200

    Add copyright to perl scripts
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- gen_funcs.pl
+++ gen_funcs.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/env perl
 
+# Copyright (c) 2018, Maxime Soulé
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 use strict;
 use warnings;
 use autodie;
