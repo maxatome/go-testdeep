0f61313970b37d7894c7b6fdc021e1886b987d26
Maxime Soulé <btik-git@scoubidou.com>
2018-08-18 21:34:32 -0600

    Add unit tests to cover all t.Helper() usages
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- cmp_funcs_misc_test.go
+++ cmp_funcs_misc_test.go
@@ -7,8 +7,12 @@
 package testdeep
 
 import (
+	"errors"
 	"fmt"
 	"testing"
+	"time"
+
+	"github.com/maxatome/go-testdeep/internal/test"
 )
 
 func ExampleCmpTrue() {
@@ -152,4 +156,66 @@ func ExampleCmpNotPanic() {
 	// checks a panic DID NOT occur: true
 	// still no panic? false
 	// last no panic? false
+}
+
+// Just to test the case where t is an interface and not a *testing.T
+// See t.Helper() issue in all tested functions.
+func TestCmpWithInterfaceT(tt *testing.T) {
+	ttt := &test.TestingFT{}
+
+	test.IsTrue(tt, CmpTrue(ttt, true, true))
+	test.IsTrue(tt, CmpFalse(ttt, false, false))
+	test.IsFalse(tt, CmpError(ttt, nil))
+	test.IsFalse(tt, CmpNoError(ttt, errors.New("error")))
+	test.IsFalse(tt, CmpPanic(ttt, func() {}, "panic")) // no panic occurred
+	test.IsTrue(tt, CmpNotPanic(ttt, func() {}))
+
+	test.IsFalse(tt, CmpAll(ttt, 0, []interface{}{12}))
+	test.IsFalse(tt, CmpAny(ttt, 0, nil))
+	test.IsFalse(tt, CmpArray(ttt, 0, [2]int{}, nil))
+	test.IsFalse(tt, CmpArrayEach(ttt, 0, nil))
+	test.IsFalse(tt, CmpBag(ttt, 0, nil))
+	test.IsFalse(tt, CmpBetween(ttt, 0, 1, 2, BoundsInIn))
+	test.IsFalse(tt, CmpCap(ttt, nil, 12))
+	test.IsFalse(tt, CmpCode(ttt, 0, func(n int) bool { return false }))
+	test.IsFalse(tt, CmpContains(ttt, 0, nil))
+	test.IsFalse(tt, CmpEmpty(ttt, 0))
+	test.IsFalse(tt, CmpGt(ttt, 0, 12))
+	test.IsFalse(tt, CmpGte(ttt, 0, 12))
+	test.IsFalse(tt, CmpHasPrefix(ttt, 0, "pipo"))
+	test.IsFalse(tt, CmpHasSuffix(ttt, 0, "pipo"))
+	test.IsFalse(tt, CmpIsa(ttt, 0, "string"))
+	test.IsFalse(tt, CmpLen(ttt, nil, 12))
+	test.IsFalse(tt, CmpLt(ttt, 0, -12))
+	test.IsFalse(tt, CmpLte(ttt, 0, -12))
+	test.IsFalse(tt, CmpMap(ttt, 0, map[int]bool{}, nil))
+	test.IsFalse(tt, CmpMapEach(ttt, 0, nil))
+	test.IsFalse(tt, CmpN(ttt, 0, 12, 0))
+	test.IsFalse(tt, CmpNaN(ttt, 0, nil))
+	test.IsFalse(tt, CmpNil(ttt, 0))
+	test.IsFalse(tt, CmpNone(ttt, 0, []interface{}{0}))
+	test.IsFalse(tt, CmpNot(ttt, 0, 0))
+	test.IsFalse(tt, CmpNotAny(ttt, 0, nil))
+	test.IsFalse(tt, CmpNotEmpty(ttt, 0, nil))
+	test.IsFalse(tt, CmpNotNaN(ttt, 0, nil))
+	test.IsFalse(tt, CmpNotNil(ttt, nil))
+	test.IsFalse(tt, CmpNotZero(ttt, 0, nil))
+	test.IsFalse(tt, CmpPPtr(ttt, 0, 12))
+	test.IsFalse(tt, CmpPtr(ttt, 0, 12))
+	test.IsFalse(tt, CmpRe(ttt, 0, "pipo", nil))
+	test.IsFalse(tt, CmpReAll(ttt, 0, "pipo", nil))
+	test.IsFalse(tt, CmpSet(ttt, 0, nil))
+	test.IsFalse(tt, CmpShallow(ttt, 0, []int{}))
+	test.IsFalse(tt, CmpSlice(ttt, 0, []int{}, nil))
+	test.IsFalse(tt, CmpSmuggle(ttt, 0, func(n int) int { return 0 }, 12))
+	test.IsFalse(tt, CmpString(ttt, 0, "pipo"))
+	test.IsFalse(tt, CmpStruct(ttt, 0, struct{}{}, nil))
+	test.IsFalse(tt, CmpSubBagOf(ttt, 0, nil))
+	test.IsFalse(tt, CmpSubMapOf(ttt, 0, map[int]bool{}, nil))
+	test.IsFalse(tt, CmpSubSetOf(ttt, 0, nil))
+	test.IsFalse(tt, CmpSuperBagOf(ttt, 0, nil))
+	test.IsFalse(tt, CmpSuperMapOf(ttt, 0, map[int]bool{}, nil))
+	test.IsFalse(tt, CmpSuperSetOf(ttt, 0, nil))
+	test.IsFalse(tt, CmpTruncTime(ttt, 0, time.Now(), time.Second))
+	test.IsFalse(tt, CmpZero(ttt, 12))
 }
