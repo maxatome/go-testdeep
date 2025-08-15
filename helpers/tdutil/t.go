e8f85aa81cc5baad4cd84c7bdc8cd71739c4ba38
Maxime Soulé <btik-git@scoubidou.com>
2025-08-15 20:43:31 +0200

    fix(tdutil): testing.T.output needs some inits starting go1.25
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- helpers/tdutil/t.go
+++ helpers/tdutil/t.go
@@ -14,7 +14,10 @@ package tdutil
 import (
 	"reflect"
 	"testing"
+	"unsafe"
 )
+
+var testingT = reflect.TypeOf(testing.T{})
 
 // T can be used in tests, to test [testing.T] behavior as it overrides
 // [testing.T.Run] method.
@@ -28,7 +31,9 @@ type tFailedNow struct{}
 // NewT returns a new [*T] instance. name is the string returned by
 // method Name.
 func NewT(name string) *T {
-	return &T{name: name}
+	t := &T{name: name}
+	t.prepareForLogs()
+	return t
 }
 
 // Run is a simplified version of [testing.T.Run] method, without edge
@@ -70,6 +75,11 @@ func (t *T) Fatalf(format string, args ...any) {
 	t.FailNow()
 }
 
+// Starting go1.18, unsafe.Add can be used instead.
+func ptrAdd(p unsafe.Pointer, x uintptr) unsafe.Pointer {
+	return unsafe.Pointer(uintptr(p) + x)
+}
+
 // CatchFailNow returns true if a [T.FailNow], [T.Fatal] or [T.Fatalf] call
 // occurred during the execution of fn.
 func (t *T) CatchFailNow(fn func()) (failNowOccurred bool) {
@@ -84,4 +94,35 @@ func (t *T) CatchFailNow(fn func()) (failNowOccurred bool) {
 
 	fn()
 	return
+}
+
+func (t *T) prepareForLogs() {
+	if _, ok := testingT.FieldByName("output"); !ok {
+		panic("testing.T.output field not found!")
+	}
+
+	// o is a new go1.25 testing.common field
+	if o, ok := testingT.FieldByName("o"); ok {
+		ot := o.Type.Elem()
+
+		// With go1.25 t.T.o needs to be set to feed t.T.output
+		type outputWriter struct {
+			c       *testing.T
+			partial []byte //nolint: unused
+		}
+		owt := reflect.TypeOf(outputWriter{})
+
+		if ot.NumField() != owt.NumField() ||
+			ot.Field(0).Name != owt.Field(0).Name || // ot.c is *testing.common
+			ot.Field(1).Type != owt.Field(1).Type {
+			panic("testing.T.outputWriter changed")
+		}
+
+		// Use ptrAdd to support go1.16 & go1.17, but starting go1.18 we can use:
+		// oAddr := (**outputWriter)(unsafe.Add(unsafe.Pointer(&t.T), o.Offset))
+		oAddr := (**outputWriter)(ptrAdd(unsafe.Pointer(&t.T), o.Offset))
+		*oAddr = &outputWriter{
+			c: &t.T,
+		}
+	}
 }
